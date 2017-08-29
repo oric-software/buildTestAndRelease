3dcb9fec0d2b5f6124aa9f59813556316c44b001
jede <jede@oric.org>
2017-08-29 22:22:11 +0200

    other test

--- make.sh
+++ make.sh
@@ -1,5 +1,14 @@
 #! /bin/sh
-sudo apt-get update -qq
-sudo apt-get install -qq git 
 sudo apt-get install -qq wine
 sudo apt-get install -qq wget
+mkdir osdk && cd osdk    
+wget http://osdk.defence-force.org/files/osdk_1_10.zip
+unzip -o osdk_1_10.zip
+cd ..
+mkdir cc65 && cd cc65
+wget http://sourceforge.net/projects/cc65/files/cc65-snapshot-win32.zip
+unzip -o cc65-snapshot-win32.zip
+cd ..
+ls -la
+ls -la osdk
+ls -la cc65
