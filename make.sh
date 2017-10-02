13b018ed5872b0c781fa67788df07c6d5af6186e
jede <jede@oric.org>
2017-10-02 22:56:29 +0200

    correction

--- make.sh
+++ make.sh
@@ -1,15 +1,19 @@
 #! /bin/sh
 
-sudo apt-get install gcc -y && apt-get install make -y && apt-get install git -y
-git clone https://github.com/cc65/cc65.git
-cd cc65
-make >> output_cc65.log
-export PATH=$PATH:$PWD
-cd ..
-cd xa
-make
-export PATH=$PATH:$PWD
-cd ..
-ls -la
-echo $PATH
+cd osdk_src/ && unzip osdk_src.zip && cd osdk/common/ && make
+cd osdk_src/ &&  cd osdk/xa/ && make
+cd osdk_src/ &&  cd osdk/filepack/ && make
+
+#sudo apt-get install gcc -y && apt-get install make -y && apt-get install git -y
+#git clone https://github.com/cc65/cc65.git
+#cd cc65
+#make >> output_cc65.log
+#export PATH=$PATH:$PWD
+#cd ..
+#cd xa
+#make
+#export PATH=$PATH:$PWD
+#cd ..
+#ls -la
+#echo $PATH
 
