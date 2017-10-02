07777922ea31a8bd6d1fb9b47869cec36e440ede
jede <jede@oric.org>
2017-10-02 23:18:35 +0200

    up

--- make.sh
+++ make.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
 cd osdk/ && unzip osdk_src.zip && cd osdk_src/osdk/main/common/ && make
-cd ../xa/ && make
-cd ../filepack/ && make
+cd ../xa/ && make && cp xa ../../../../
+cd ../filepack/ && make && cp filepack ../../../../
 
-#sudo apt-get install gcc -y && apt-get install make -y && apt-get install git -y
-#git clone https://github.com/cc65/cc65.git
+cd ../../../../
+
+git clone https://github.com/cc65/cc65.git
 #cd cc65
 #make >> output_cc65.log
 #export PATH=$PATH:$PWD
