cf2e4f64209c320ba5727f6fbe6bd13109148961
jede <jede@oric.org>
2017-10-02 22:04:08 +0200

    Add osdk (it contains XA, common and filepack

--- make.sh
+++ make.sh
@@ -2,11 +2,11 @@
 sudo apt-get install -qq git
 git clone https://github.com/cc65/cc65.git
 cd cc65
-make
+make >> output_cc65.log
 export PATH=$PATH:$PWD
 cd ..
 cd xa
-make all
+make
 export PATH=$PATH:$PWD
 cd ..
 ls -la
