1d15c959a0b887d5976a5e5c2e5cbefae89776c5
jede <jede@oric.org>
2017-10-08 22:21:54 +0200

    adding ld

--- make.sh
+++ make.sh
@@ -15,6 +15,7 @@ cp bin/cl65 /home/travis/bin && chmod +x /home/travis/bin/cl65
 cp bin/ca65 /home/travis/bin && chmod +x /home/travis/bin/ca65
 cp bin/cc65 /home/travis/bin && chmod +x /home/travis/bin/cc65
 cp bin/ar65 /home/travis/bin && chmod +x /home/travis/bin/ar65
+cp bin/ld65 /home/travis/bin && chmod +x /home/travis/bin/ld65
 cd ..
 
 git clone https://github.com/jedeoric/telemon.git
