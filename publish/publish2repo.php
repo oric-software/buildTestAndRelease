a4b645dcadb202be2a6ce976c8b1cff08d7f870e
jede <jede@oric.org>
2017-10-25 19:48:03 +0200

    add publish2repo.php

---
+++ publish/publish2repo.php
@@ -0,0 +1,25 @@
+<?
+$file=$argv[1];
+$hash=$argv[2];
+$cpu=$argv[3];
+$url="https://cdn.oric.org/publish.php?hash=$hash&path=/home/oricoujr/www/ftp/orix/dists/orix1/official/$cpu/$file";
+$image_path="/volume1/web/cdn/oric_detour.png";
+$file_on_dir_not_url="";
+$image = fopen($image_path, "rb");
+
+ $curl = curl_init();
+ curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 2);
+ curl_setopt($curl, CURLOPT_HEADER, false);
+ curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+ curl_setopt($curl, CURLOPT_BINARYTRANSFER, 1);
+ curl_setopt($curl, CURLOPT_URL, $url);
+
+ curl_setopt($curl, CURLOPT_PUT, 1);
+ curl_setopt($curl, CURLOPT_INFILE, $image);
+ 
+ 
+ curl_setopt($curl, CURLOPT_INFILESIZE, filesize($image_path));
+//
+ $result = curl_exec($curl);
+ curl_close($curl); 
+?>
