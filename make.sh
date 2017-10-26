c650b76fbe4b3e793a7ffd279aac35b8809a3888
jede <jede@oric.org>
2017-10-26 21:33:48 +0200

    update co65

--- make.sh
+++ make.sh
@@ -20,6 +20,7 @@ cp bin/ca65 /home/travis/bin && chmod +x /home/travis/bin/ca65
 cp bin/cc65 /home/travis/bin && chmod +x /home/travis/bin/cc65
 cp bin/ar65 /home/travis/bin && chmod +x /home/travis/bin/ar65
 cp bin/ld65 /home/travis/bin && chmod +x /home/travis/bin/ld65
+cp bin/co65 /home/travis/bin && chmod +x /home/travis/bin/co65
 cd ..
 mkdir ../../../telemon/
 git clone https://github.com/jedeoric/telemon.git ../../../telemon/ 
