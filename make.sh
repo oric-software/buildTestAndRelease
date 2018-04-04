b278ce4afb309e0d72576e7f7071fd7d43bb5935
jede <jede@oric.org>
2018-04-04 09:27:59 +0200

    test

--- make.sh
+++ make.sh
@@ -9,6 +9,7 @@ HOMEDIRBIN=/home/travis/bin/
 cd osdk/ && unzip osdk_src.zip && cd osdk_src/osdk/main/common/ && make
 cd ../xa/ && make && cp xa /home/travis/bin && chmod +x /home/travis/bin/xa
 cd ../filepack/ && make && cp filepack /home/travis/bin && chmod +x /home/travis/bin/filepack
+ls Ym2Mym/ && ls Ym2Mym/src
 cd ../Ym2Mym/ && make  && cp Ym2Mym/ym2mym /home/travis/bin && chmod +x /home/travis/bin/ym2mym
 ls /home/travis/bin
 echo $PATH
